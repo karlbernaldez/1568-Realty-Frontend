bf8db3cf30ed706dc37011c22bb9c0c5061d3ab1
Bryan N. Lomerio <102351612+Aninooo@users.noreply.github.com>
2024-03-25 16:43:18 +0800

    fix: fix the title

--- src/pages/FAQs/index.jsx
+++ src/pages/FAQs/index.jsx
@@ -8,7 +8,7 @@ export default function FAQsPage() {
   return (
     <>
       <Helmet>
-        <title>Real State</title>
+        <title>1568 Realty</title>
         <meta name="description" content="Web site created using create-react-app" />
       </Helmet>
       <div className="flex md:flex-col items-start w-full pr-14 gap-6 md:pr-5 bg-gray-100_01">
