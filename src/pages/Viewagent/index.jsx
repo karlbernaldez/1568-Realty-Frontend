afc35a7a0e88b7997308b134be1dd00a6f59f8f4
Bryan N. Lomerio <102351612+Aninooo@users.noreply.github.com>
2024-03-26 09:41:46 +0800

    Update index.jsx

--- src/pages/Viewagent/index.jsx
+++ src/pages/Viewagent/index.jsx
@@ -7,7 +7,7 @@ export default function ViewagentPage() {
   return (
     <>
       <Helmet>
-        <title>Bryan's Application1</title>
+        <title>1568 Realstate</title>
         <meta name="description" content="Web site created using create-react-app" />
       </Helmet>
       <div className="flex md:flex-col items-start w-full gap-[25px] bg-gray-100">
